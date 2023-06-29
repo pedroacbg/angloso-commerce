fabc16fd5211226f0704eabe4fb43f48b2c65d1e
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-06-29 14:41:50 -0300

    backend requests with axios

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,13 +2,22 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from "../../../services/product-service";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
+import * as productService from "./../../../services/product-service";
 
 export default function ProductDetails() {
   const params = useParams();
-  const product = productService.findById(Number(params.productId));
+
+  const [product, setProduct] = useState<ProductDTO>();
+
+  useEffect(() => {
+    productService.findById(Number(params.productId)).then((response) => {
+      setProduct(response.data);
+    });
+  }, []);
 
   return (
     <>
