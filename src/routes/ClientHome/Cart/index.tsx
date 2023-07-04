86dd7de7afccf0a74bb7e36b06bcfb29e0cc2fa9
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-07-03 23:14:33 -0300

    increase and decrease cart item quantity

--- src/routes/ClientHome/Cart/index.tsx
+++ src/routes/ClientHome/Cart/index.tsx
@@ -12,6 +12,16 @@ export default function Cart() {
     setCart(cartService.getCart());
   }
 
+  function handleIncreaseItem(productId: number) {
+    cartService.increaseItem(productId);
+    setCart(cartService.getCart());
+  }
+
+  function handleDecreaseItem(productId: number) {
+    cartService.decreaseItem(productId);
+    setCart(cartService.getCart());
+  }
+
   return (
     <main>
       <section id="cart-container-section" className="dsc-container">
@@ -33,9 +43,19 @@ export default function Cart() {
                   <div className="dsc-cart-item-description">
                     <h3>{item.name}</h3>
                     <div className="dsc-cart-item-quantity-container">
-                      <div className="dsc-cart-item-quantity-btn">-</div>
+                      <div
+                        className="dsc-cart-item-quantity-btn"
+                        onClick={() => handleDecreaseItem(item.productId)}
+                      >
+                        -
+                      </div>
                       <p>{item.quantity}</p>
-                      <div className="dsc-cart-item-quantity-btn">+</div>
+                      <div
+                        className="dsc-cart-item-quantity-btn"
+                        onClick={() => handleIncreaseItem(item.productId)}
+                      >
+                        +
+                      </div>
                     </div>
                   </div>
                 </div>
