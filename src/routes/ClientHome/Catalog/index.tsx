5554779b89ce3bdd899ad48b809132ef0d5175b9
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-07-04 15:39:53 -0300

    catalog next page

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -12,6 +12,7 @@ type QueryParams = {
 };
 
 export default function Catalog() {
+  const [isLastPage, setIsLastPage] = useState(false);
   const [products, setProducts] = useState<ProductDTO[]>([]);
   const [queryParams, setQueryParams] = useState<QueryParams>({
     page: 0,
@@ -22,12 +23,20 @@ export default function Catalog() {
     productService
       .findPageRequest(queryParams.page, queryParams.name)
       .then((response) => {
-        setProducts(response.data.content);
+        const nextPage = response.data.content;
+        setProducts(products.concat(nextPage));
+        setIsLastPage(response.data.last);
       });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [queryParams]);
 
   function handleSearch(searchText: string) {
-    setQueryParams({ ...queryParams, name: searchText });
+    setProducts([]);
+    setQueryParams({ ...queryParams, page: 0, name: searchText });
+  }
+
+  function handleNextPageClick() {
+    setQueryParams({ ...queryParams, page: queryParams.page + 1 });
   }
 
   return (
@@ -40,7 +49,11 @@ export default function Catalog() {
               <CatalogCard key={item.id} product={item} />
             ))}
           </div>
-          <ButtonNextPage />
+          {!isLastPage && (
+            <div onClick={handleNextPageClick}>
+              <ButtonNextPage />
+            </div>
+          )}
         </section>
       </main>
     </>
