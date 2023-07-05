9662d5b82c7ac4a9f717fdcf386bce8a010bf947
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-07-05 14:16:15 -0300

    admin page layout

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,6 +1,7 @@
 export const BASE_URL = "http://localhost:8080";
 
 export const CART_KEY = "com.angloso.angloso-commerce/Cart";
+export const TOKEN_KEY = "com.angloso.angloso-commerce/Token";
 
 export const CLIENT_ID = "myclientid";
 export const CLIENT_SECRET = "myclientsecret";
