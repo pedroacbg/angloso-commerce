f7e4b8cf04054314a737d002f9758c044b096b22
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-07-05 13:45:19 -0300

    login form and request

--- src/utils/system.ts
+++ src/utils/system.ts
@@ -1,3 +1,6 @@
 export const BASE_URL = "http://localhost:8080";
 
 export const CART_KEY = "com.angloso.angloso-commerce/Cart";
+
+export const CLIENT_ID = "myclientid";
+export const CLIENT_SECRET = "myclientsecret";
