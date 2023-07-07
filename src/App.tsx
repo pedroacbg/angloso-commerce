8f2042b29c4563449adb35e9d286c932edd6d65f
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-07-07 14:13:48 -0300

    access control route level

--- src/App.tsx
+++ src/App.tsx
@@ -34,7 +34,7 @@ function App() {
           <Route
             path="/admin/"
             element={
-              <PrivateRoute>
+              <PrivateRoute roles={["ROLE_ADMIN"]}>
                 <Admin />
               </PrivateRoute>
             }
